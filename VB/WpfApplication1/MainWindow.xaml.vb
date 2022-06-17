204d8f2aa206fa160820ae13be33cbe988b4856c
DevExpressExampleBot <fakeEmail>
2022-06-17 15:04:21 +0400

    Source auto update [skip ci]

--- VB/WpfApplication1/MainWindow.xaml.vb
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -1,38 +1,26 @@
-﻿Imports DevExpress.Xpf.Docking
+Imports DevExpress.Xpf.Docking
 Imports DevExpress.Xpf.Layout.Core
 Imports DevExpress.Xpf.Ribbon
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace WpfApplication1
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits DXRibbonWindow
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
         Private Sub biMDI_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-            ChildDocumentGroup.MDIStyle = MDIStyle.MDI
+            Me.ChildDocumentGroup.MDIStyle = MDIStyle.MDI
         End Sub
 
         Private Sub biTabbed_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-            ChildDocumentGroup.MDIStyle = MDIStyle.Tabbed
+            Me.ChildDocumentGroup.MDIStyle = MDIStyle.Tabbed
         End Sub
     End Class
 End Namespace
